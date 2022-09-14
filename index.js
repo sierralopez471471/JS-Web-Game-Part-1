5630c38439bae4a2250198fb9c83cf5d05ae884f
Sierra Lopez <sierralopez1006@gmail.com>
2022-09-13 19:02:47 -0700

    newItem function added and implemented

--- index.js
+++ index.js
@@ -8,11 +8,28 @@ function newImage(url, left, bottom){
     return object 
 }
 
+function newItem(url, left, bottom){
+    let object = document.createElement('img')
+    object.src = url
+    object.style.position = 'fixed'
+    object.style.left = left + 'px'
+    object.style.bottom = bottom + 'px'
+    document.body.append(object)
+    object.addEventListener('click', function(){
+        object.remove()
+    })
+    return object 
+}
+
 newImage('assets/green-character.gif', 100, 100)
 newImage('assets/pine-tree.png', 450, 200)
 newImage('assets/tree.png', 200, 300)
 newImage('assets/pillar.png', 350, 100)
 newImage('assets/crate.png', 150, 200)
 newImage('assets/well.png', 500, 425)
+
+newItem('assets/sword.png', 500, 405)
+newItem('assets/sheild.png', 165, 185)
+newItem('assets/staff.png', 600, 100)
 
 
